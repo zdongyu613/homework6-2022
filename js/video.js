e8105d9f3b09082427a320d11b981729bcdb4582
JKurenai <anysvph@gmail.com>
2022-04-05 15:31:35 -0400

    Video Control Complete!

--- js/video.js
+++ js/video.js
@@ -8,6 +8,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play()
+	document.querySelector("#volume").innerHTML = video.volume*100 + "%"
 });
 
 document.querySelector("#pause").addEventListener("click", function (){
@@ -27,5 +28,38 @@ document.querySelector("#faster").addEventListener("click", function (){
 
 document.querySelector("#skip").addEventListener("click", function (){
 	console.log("Skip ahead by 15s")
-	video.currentTime += 15
+	var t = video.currentTime + 15
+	if(t>=video.duration){
+		video.currentTime = 0
+	}
+	else{
+		video.currentTime = t
+	}
+	console.log(video.currentTime)
+})
+
+document.querySelector("#mute").addEventListener("click", function (){
+	video.muted = video.muted === false;
+	if(video.muted){
+		document.querySelector("#mute").innerHTML="Unmute"
+		console.log("video muted")
+	}else {
+		document.querySelector("#mute").innerHTML="Mute"
+		console.log("video unmuted")
+	}
+})
+
+document.querySelector("#slider").onchange = function (){
+	video.volume = this.value/100
+	document.querySelector("#volume").innerHTML = this.value + "%"
+	console.log("the current volume is "+this.value+"%")
+}
+
+document.querySelector("#vintage").addEventListener("click", function (){
+	video.className = "oldSchool"
+	console.log("Old School Style!!!")
+})
+document.querySelector("#orig").addEventListener("click", function (){
+	video.className = "video"
+	console.log("Back to original style.")
 })
